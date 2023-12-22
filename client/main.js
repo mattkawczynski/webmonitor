ced9034a81e65cd36e95481ac5ef4e9254b637fc
mattkawczynski <mateuszkawczynski@gmail.com>
2023-12-22 01:21:58 +0100

    Added custom titleBar and styling

--- client/main.js
+++ client/main.js
@@ -1,80 +1,81 @@
-'use strict'
+'use strict';
 
 const { app, BrowserWindow, ipcMain, Menu, Tray, session } = require('electron');
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 require('dotenv').config();
-app.commandLine.appendSwitch('disable-web-security')
+
+app.commandLine.appendSwitch('disable-web-security');
 
 let mainWindow;
-let dev = false;
+let dev = process.env.NODE_ENV === 'development';
 
-if (process.env.NODE_ENV !== undefined && process.env.NODE_ENV === 'development') {
-  dev = true
-}
+app.on('ready', createMainWindow);
 
-app.on('ready', createMainWindow)
+
 function createMainWindow() {
   mainWindow = new BrowserWindow({
-    //backgroundColor: '#2e2c29',
+    frame: false,
     width: 400,
     height: 768,
-    //frame: false,
-    //titleBarStyle: 'hiddenInset',
+    minHeight: 400,
+    minWidth: 1000,
+    titleBarStyle: 'hidden',
     titleBarOverlay: {
-        color: '#2f3241',
-        symbolColor: '#74b1be',
-        height: 30,
+      color: '#2f3241',
+      symbolColor: '#74b1be',
+      height: 30,
     },
     trafficLightPosition: { x: 10, y: 10 },
     darkTheme: true,
     webPreferences: {
-        devTools: true,
-        nodeIntegration: true , 
-        contextIsolation: false
+      devTools: dev,
+      nodeIntegration: true,
+      contextIsolation: false,
     },
-    show: false
+    show: false,
   });
-  
+
   if (dev) {
-    const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
+    const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 
     installExtension(REACT_DEVELOPER_TOOLS)
-      .catch(err => console.log('Error loading React DevTools: ', err))
-    mainWindow.webContents.openDevTools()
-  }
-  // and load the index.html of the app.
-  let indexPath
-  
-  if (dev && process.argv.indexOf('--noDevServer') === -1) {
-    indexPath = url.format({
-      protocol: 'http:',
-      host: 'localhost:8080',
-      pathname: 'index.html',
-      slashes: true
-    })
+      .catch(err => console.log('Error loading React DevTools: ', err));
+
+    mainWindow.webContents.openDevTools();
+    
   } else {
-    indexPath = url.format({
-      protocol: 'file:',
-      pathname: path.join(__dirname, 'dist', 'index.html'),
-      slashes: true
-    })
+    Menu.setApplicationMenu(null);
   }
 
-  mainWindow.loadURL(indexPath)
-  mainWindow.maximize()
+  const indexPath = dev
+    ? url.format({
+        protocol: 'http:',
+        host: 'localhost:8080',
+        pathname: 'index.html',
+        slashes: true,
+      })
+    : url.format({
+        protocol: 'file:',
+        pathname: path.join(__dirname, 'dist', 'index.html'),
+        slashes: true,
+      });
+
+  mainWindow.loadURL(indexPath);
+  mainWindow.maximize();
 
   mainWindow.on('close', (event) => {
     event.preventDefault();
     mainWindow.hide();
   });
 
-  mainWindow.on('closed', function() {
-    mainWindow = null
-  })
-
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+  const mainMenu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(mainMenu);
   const iconPath = path.join(__dirname, './src/assets/windows-icon@2x.png');
-  const tray = new Tray(iconPath)
+  const tray = new Tray(iconPath);
   tray.setContextMenu(buildTrayMenu());
   tray.on('click', handleTrayClick);
 }
@@ -82,15 +83,46 @@ function createMainWindow() {
 function buildTrayMenu() {
   return Menu.buildFromTemplate([
     {
-      label: 'Close',
-      click: () => {
-        mainWindow.close();
-      }
-    }
+      label: "Menu",
+      submenu: [
+          {
+              label: "Quit",
+              accelerator: (() => {
+                  if (process.platform === 'darwin') {
+                      return 'Command+Q';
+                  } else {
+                      return 'Ctrl+Q';
+                  }
+              })(),
+              click() {
+                  app.quit();
+              }
+          }
+      ]
+  }
   ]);
 }
-
-function handleTrayClick(event, bounds) {
+const menuTemplate = [
+  {
+      label: "Files",
+      submenu: [
+          {
+              label: "Quit",
+              accelerator: (() => {
+                  if (process.platform === 'darwin') {
+                      return 'Command+Q';
+                  } else {
+                      return 'Ctrl+Q';
+                  }
+              })(),
+              click() {
+                  app.quit();
+              }
+          }
+      ]
+  }
+]
+function handleTrayClick() {
   if (mainWindow.isVisible()) {
     mainWindow.hide();
   } else {
@@ -100,6 +132,6 @@ function handleTrayClick(event, bounds) {
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
